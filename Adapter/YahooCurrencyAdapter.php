4f9a9f58f4164e5945722ea7fcc22ff28b8841a7
Jonas Dambacher <mail@jonas-dambacher.de>
2016-01-19 10:49:27 +0100

    Changed line indentation to spaces

--- Adapter/YahooCurrencyAdapter.php
+++ Adapter/YahooCurrencyAdapter.php
@@ -16,99 +16,99 @@ class YahooCurrencyAdapter extends AbstractCurrencyAdapter
      */
     private $yahooUrl;
 
-	/**
-	 * @var array
-	 */
-	private $currencyCodes = array();
+    /**
+     * @var array
+     */
+    private $currencyCodes = array();
 
 
-	/**
-	 * Set the Yahoo! url.
-	 *
-	 * @param string $url
-	 */
-	public function setYahooUrl($url)
-	{
-		$this->yahooUrl = $url;
-	}
+    /**
+     * Set the Yahoo! url.
+     *
+     * @param string $url
+     */
+    public function setYahooUrl($url)
+    {
+        $this->yahooUrl = $url;
+    }
 
-	/**
+    /**
      * Init object storage
      */
     public function attachAll()
     {
-		foreach ($this->managedCurrencies as $managedCurrency) {
-			$this->addCurrency($managedCurrency);
-		}
+        foreach ($this->managedCurrencies as $managedCurrency) {
+            $this->addCurrency($managedCurrency);
+        }
 
-		$defaultRate = 1;
+        $defaultRate = 1;
 
-		// Add default currency (euro in this example)
-		$euro = new $this->currencyClass;
-		$euro->setCode('EUR');
-		$euro->setRate($defaultRate);
+        // Add default currency (euro in this example)
+        $euro = new $this->currencyClass;
+        $euro->setCode('EUR');
+        $euro->setRate($defaultRate);
 
-		$this[$euro->getCode()] = $euro;
+        $this[$euro->getCode()] = $euro;
 
-		// Build YQL query
-		$strCodes = '';
-		foreach ($this->currencyCodes as $index=>$currencyCode) {
-			$strCodes .= "'EUR".$currencyCode."'";
-			if ($index != count($this->currencyCodes) - 1) {
-				$strCodes .= ", ";
-			}
-		}
+        // Build YQL query
+        $strCodes = '';
+        foreach ($this->currencyCodes as $index=>$currencyCode) {
+            $strCodes .= "'EUR".$currencyCode."'";
+            if ($index != count($this->currencyCodes) - 1) {
+                $strCodes .= ", ";
+            }
+        }
 
-		$yqlQuery = "select id,Rate from yahoo.finance.xchange where pair in (".$strCodes.")";
+        $yqlQuery = "select id,Rate from yahoo.finance.xchange where pair in (".$strCodes.")";
 
-		$args = array(
-			'q' => $yqlQuery,
-			'format' => "json",
-			'env' => "store://datatables.org/alltableswithkeys",
-		);
+        $args = array(
+            'q' => $yqlQuery,
+            'format' => "json",
+            'env' => "store://datatables.org/alltableswithkeys",
+        );
 
-		$yqlQueryURL = $this->yahooUrl
-			. "?q=" . urlencode($yqlQuery)
-			. "&format=json"
-			. "&env=store://datatables.org/alltableswithkeys";
+        $yqlQueryURL = $this->yahooUrl
+            . "?q=" . urlencode($yqlQuery)
+            . "&format=json"
+            . "&env=store://datatables.org/alltableswithkeys";
 
-		$ch = curl_init();
-		curl_setopt($ch, CURLOPT_URL, $yqlQueryURL);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-		$json = curl_exec($ch);
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $yqlQueryURL);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+        $json = curl_exec($ch);
 
-		// Convert JSON response to PHP object
-		$data = json_decode($json);
-		$results = $data->query->results->rate;
+        // Convert JSON response to PHP object
+        $data = json_decode($json);
+        $results = $data->query->results->rate;
 
-		// Check if query was okay and result is given
-		if (is_null($results)) {
-			new \RuntimeException('YQL query failed!');
-		}
+        // Check if query was okay and result is given
+        if (is_null($results)) {
+            new \RuntimeException('YQL query failed!');
+        }
 
-		$currencies = array();
+        $currencies = array();
 
-		foreach ($results as $row) {
-			$code = substr($row->id, 3);
-			$rate = $row->Rate;
+        foreach ($results as $row) {
+            $code = substr($row->id, 3);
+            $rate = $row->Rate;
 
-			$currencies[$code] = $rate;
-		}
+            $currencies[$code] = $rate;
+        }
 
         foreach ($currencies as $code => $rate) {
-			if (in_array($code, $this->managedCurrencies)) { // you can check if the currency is in the managed currencies
-				$currency = new $this->currencyClass;
-				$currency->setCode($code);
-				$currency->setRate($rate);
+            if (in_array($code, $this->managedCurrencies)) { // you can check if the currency is in the managed currencies
+                $currency = new $this->currencyClass;
+                $currency->setCode($code);
+                $currency->setRate($rate);
 
                 $this[$currency->getCode()] = $currency;
             }
-		}
+        }
 
         // get the default rate from the default currency defined in the configuration
         if (isset($this[$this->defaultCurrency])) {
-			$defaultRate = $this[$this->defaultCurrency]->getRate();
-		}
+            $defaultRate = $this[$this->defaultCurrency]->getRate();
+        }
 
         // convert rates according to the default one.
         $this->convertAll($defaultRate);
@@ -122,13 +122,13 @@ class YahooCurrencyAdapter extends AbstractCurrencyAdapter
         return 'yahoo';
     }
 
-	/**
-	 * Add currency to the query
-	 *
-	 * @param $code
-	 */
-	private function addCurrency($code) {
-		$this->currencyCodes[] = $code;
-	}
+    /**
+     * Add currency to the query
+     *
+     * @param $code
+     */
+    private function addCurrency($code) {
+        $this->currencyCodes[] = $code;
+    }
 
 }
